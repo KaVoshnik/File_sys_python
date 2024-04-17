1c8f47bbe2394a56ab750dc787a5228cf1be744b
Kirill <112951242+KaVoshnik@users.noreply.github.com>
2024-04-17 16:42:29 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,6 +8,7 @@ def sort_data_by_type(input_file, output_path, file_prefix):
     with open(input_file, 'r') as file:
         for line in file:
             line = line.strip()
+
             try:
                 number = int(line)
                 integers_output.write(f'{number} ')
@@ -24,11 +25,12 @@ def sort_data_by_type(input_file, output_path, file_prefix):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_file', help='Путь к файлу со значнениями')
+    parser.add_argument('input_file', help='Путь к входному файлу')
     parser.add_argument('-o', '--output_path', help='Путь для сохранения результатов', default='./')
     parser.add_argument('-p', '--file_prefix', help='Префикс имен выходных файлов', default='')
-    #parser.add_argument('-a', '--append_mode', help='Префикс для добавлнния в существующий файл', action='append')
-    #Допилить
-    args = parser.parse_args()
 
-    sort_data_by_type(args.input_file, args.output_path, args.file_prefix)
+    try:
+        args = parser.parse_args()
+        sort_data_by_type(args.input_file, args.output_path, args.file_prefix)
+    except Exception as e:
+        print(f'Произошла ошибка: {e}')
